59402e60b69f8070332aee9880f64a5654cc04b2
Muhammad Ateeq <ateeq@teknuk.com>
2016-01-14 15:19:29 +0500

    added responed to js

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -2,5 +2,8 @@ class HooksController < ApplicationController
   def payload
     # push = JSON.parse(request.body.read)
     # puts "I got some JSON: #{push.inspect}"
+    respond_to do |format|
+      format.js
+    end
   end
 end
