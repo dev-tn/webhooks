95d2eb4be8bf1a487fa8552003cbc957d101a0ed
Muhammad Ateeq <ateeq@teknuk.com>
2016-01-14 15:03:09 +0500

    invalid token auth test

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,6 +1,6 @@
 class HooksController < ApplicationController
   def payload
-    push = JSON.parse(request.body.read)
-    puts "I got some JSON: #{push.inspect}"
+    # push = JSON.parse(request.body.read)
+    # puts "I got some JSON: #{push.inspect}"
   end
 end
